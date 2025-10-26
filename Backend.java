02ee7d5ac4c5584477eccb044e38a3d30cc8956f
unknown <huy15a3k23@cvp.vn>
2025-10-26 11:18:43 +0700

    Commit tuần 6

---
+++ Backend.java
@@ -0,0 +1,30 @@
+import java.util.List;
+public class Backend {
+    private List<String> data;
+
+    public Backend(List<String> data) {
+        this.data = data;
+    }
+
+    }
+
+    public List<String> getData() {
+        return data;
+    }
+
+    public void setData(List<String> data) {
+        this.data = data;
+    }
+
+    public String processData() {
+        // Simulate data processing
+        StringBuilder processed = new StringBuilder();
+        for (String item : data) {
+            processed.append(item.toUpperCase()).append(" ");
+        }
+        return processed.toString().trim();
+    }
+
+    public void addData(String item) {
+        data.add(item);
+    }
